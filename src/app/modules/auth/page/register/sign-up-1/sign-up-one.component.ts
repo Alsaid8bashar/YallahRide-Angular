3f70f657e30c3315d63c6e3c891a197ae1f72758
hasanms97 <shnaqhassan@hotmail.com>
2023-05-27 21:52:48 +0300

    playing sport

---
+++ src/app/modules/auth/page/register/sign-up-1/sign-up-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-one',
+  templateUrl: './sign-up-one.component.html',
+  styleUrls: ['./sign-up-one.component.css']
+})
+export class SignUpOneComponent {
+
+}
---
+++ src/app/modules/auth/page/register/two-factor-auth/two-factor-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-factor-auth',
+  templateUrl: './two-factor-auth.component.html',
+  styleUrls: ['./two-factor-auth.component.css']
+})
+export class TwoFactorAuthComponent {
+
+}
