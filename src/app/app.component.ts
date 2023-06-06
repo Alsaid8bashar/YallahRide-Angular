3403d1c84e0021ecf43881e222b206bc4f7f04de
Alsaid8bashar <bmalsaiedahmad19@cit.just.ed>
2023-06-06 23:50:19 +0300

    Added dynamic-script-loader-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DynamicScriptLoaderService} from "./shared/service/dynamic-script-loader-service.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'YallahRide-Angular';
+
+
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) {
+  }
+
+  private loadScripts() {
+    this.dynamicScriptLoader.load('bootstrap.bundle.min', 'choices',  'tiny-slider', 'flatpickr', 'glightbox','functions').then(data => {
+    }).catch(error => console.log(error));
+  }
+
+  ngOnInit() {
+    this.loadScripts();
+  }
 }
---
+++ src/app/shared/service/dynamic-script-loader-service.service.ts
@@ -0,0 +1,70 @@
+import {Injectable} from '@angular/core';
+
+interface Scripts {
+  name: string;
+  src: string;
+}
+
+export const ScriptStore: Scripts[] = [
+  {name: 'functions', src: './assets/js/functions.js'},
+  {name: 'tiny-slider', src: './assets/vendor/tiny-slider/tiny-slider.js'},
+  {name: 'bootstrap.bundle.min', src: './assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js'},
+  {name: 'flatpickr', src: './assets/vendor/flatpickr/js/flatpickr.min.js'},
+  {name: 'glightbox', src: './assets/vendor/glightbox/js/glightbox.js'},
+  {name: 'choices', src: './assets/vendor/choices/js/choices.min.js'},
+];
+
+declare var document: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DynamicScriptLoaderService {
+
+  private scripts: any = {};
+
+  constructor() {
+    ScriptStore.forEach((script: any) => {
+      this.scripts[script.name] = {
+        loaded: false,
+        src: script.src
+      };
+    });
+  }
+
+  load(...scripts: string[]) {
+    const promises: any[] = [];
+    scripts.forEach((script) => promises.push(this.loadScript(script)));
+    return Promise.all(promises);
+  }
+
+  loadScript(name: string) {
+    return new Promise((resolve, reject) => {
+      if (!this.scripts[name].loaded) {
+        //load script
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = this.scripts[name].src;
+        if (script.readyState) {  //IE
+          script.onreadystatechange = () => {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+              script.onreadystatechange = null;
+              this.scripts[name].loaded = true;
+              resolve({script: name, loaded: true, status: 'Loaded'});
+            }
+          };
+        } else {  //Others
+          script.onload = () => {
+            this.scripts[name].loaded = true;
+            resolve({script: name, loaded: true, status: 'Loaded'});
+          };
+        }
+        script.onerror = (error: any) => resolve({script: name, loaded: false, status: 'Loaded'});
+        document.getElementsByTagName('head')[0].appendChild(script);
+      } else {
+        resolve({script: name, loaded: true, status: 'Already Loaded'});
+      }
+    });
+  }
+
+}
