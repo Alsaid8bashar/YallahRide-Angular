f7231384774fec41c5b49ccd6d900f33f72c13ee
hasanms97 <shnaqhassan@hotmail.com>
2023-06-11 20:25:27 +0300

    fixed the redundant code used in every component to loead the javascripts files dynamically

--- src/app/layout/account-layout/account-layout.component.ts
+++ src/app/layout/account-layout/account-layout.component.ts
@@ -1,4 +1,5 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 import {DynamicScriptLoaderService} from "../../shared/service/dynamic-script-loader-service.service";
 
 @Component({
@@ -6,22 +7,27 @@ import {DynamicScriptLoaderService} from "../../shared/service/dynamic-script-lo
   templateUrl: './account-layout.component.html',
   styleUrls: ['./account-layout.component.css']
 })
-export class AccountLayoutComponent {
+export class AccountLayoutComponent implements AfterViewInit {
+  constructor(private router: Router,private dynamicScriptLoader: DynamicScriptLoaderService) {
 
-  // constructor(private dynamicScriptLoader: DynamicScriptLoaderService) {
-  // }
-  //
-  //
-  // private loadScripts() {
-  //   this.dynamicScriptLoader.load('bootstrap.bundle.min', 'choices', 'flatpickr', 'glightbox','functions','aos').then(data => {
-  //   }).catch(error => console.log(error));
-  // }
-  //
-  // ngOnInit(): void {
-  //   this.loadScripts();
-  // }
-  // ngOnDestroy(): void {
-  //   this.dynamicScriptLoader.unload('bootstrap.bundle.min', 'choices', 'flatpickr', 'glightbox', 'functions', 'aos');
-  // }
+  }
 
+  ngAfterViewInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.unloadScripts();
+        this.loadScripts();
+      }
+    });
+  }
+
+  private loadScripts() {
+    this.dynamicScriptLoader.load('bootstrap.bundle.min', 'choices', 'flatpickr', 'glightbox','functions','aos').then(data => {console.log(data)
+    }).catch(error => console.log(error));
+  }
+  private unloadScripts() {
+    this.dynamicScriptLoader.unload('bootstrap.bundle.min', 'choices', 'flatpickr', 'glightbox','functions','aos').then(data => {console.log(data)
+    }).catch(error => console.log(error));
+    console.log("Hasan")
+  }
 }
