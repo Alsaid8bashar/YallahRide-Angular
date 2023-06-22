9b8ce4e3380451518eccddc7911330ec2eaeb90c
Alsaid8bashar <bmalsaiedahmad19@cit.just.ed>
2023-06-22 16:59:14 +0300

    Fixed aws bug

--- src/app/layout/layout-user/header/header.component.ts
+++ src/app/layout/layout-user/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../../data/service/user.service";
+import {FileStorageService} from "../../../shared/service/files-storage.service";
+import {HttpResponse} from "@angular/common/http";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  displayedFileUrl: SafeUrl;
+
+  constructor(private userService: UserService, private fileService: FileStorageService, private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+    this.getUserImage();
+  }
+
+
+  getUserImage() {
+    const userImage = this.userService.getUserSubject().imagePath;
+    console.error(userImage);
+    this.fileService.getFile(userImage).subscribe(
+      (response: HttpResponse<Blob>) => {
+        this.displayedFileUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(response.body));
+      },
+      error => {
+        console.log('Error fetching file:', error);
+      }
+    );
+  }
 }
